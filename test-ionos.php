2c2077182c987690ba5f969bf0f350aa7c221b1a
aliajjoub2 <83879405+aliajjoub2@users.noreply.github.com>
2024-12-05 13:23:25 +0100

    3

--- test-ionos.php
+++ test-ionos.php
@@ -2,9 +2,9 @@
 /*
 Plugin Name: Hello-Ali
 Author: Test Doe
-Author URI: https://example.com
-Update URI: https://example.com
-Plugin URI: https://example.com
+Author URI: https://ali-ajjoub.de
+Update URI: https://github.com/aliajjoub2/test-ionos/blob/main/test-ionos.php
+Plugin URI: https://ionos.de
 Description: This is a test plugin to test updates from GitHub.
 Version: 1.0.0
 */
@@ -27,35 +27,35 @@ add_filter('update_plugins_example.com', function ($update, $plugin_data, $plugi
         return $update;
 
         // Fetch the latest release information from GitHub API
-        $api_url = "https://api.github.com/repos/{$github_user}/{$github_repo}/releases/latest";
-        $response = wp_remote_get($api_url);
+        // $api_url = "https://api.github.com/repos/{$github_user}/{$github_repo}";
+        // $response = wp_remote_get($api_url);
 
-        if (is_wp_error($response)) {
-            return $update; // Return existing update data if there's an error
-        }
+        // if (is_wp_error($response)) {
+        //     return $update; // Return existing update data if there's an error
+        // }
 
-        $release_data = json_decode(wp_remote_retrieve_body($response), true);
+        // $release_data = json_decode(wp_remote_retrieve_body($response), true);
 
         // Ensure the response contains the required data
-        if (!empty($release_data['tag_name']) && !empty($release_data['zipball_url'])) {
-            $latest_version = $release_data['tag_name'];
-            $current_version = $plugin_data['Version'];
+        // if (!empty($release_data['tag_name']) && !empty($release_data['zipball_url'])) {
+        //     $latest_version = $release_data['tag_name'];
+        //     $current_version = $plugin_data['Version'];
 
-            // Compare the current version with the latest version
-            if (version_compare($current_version, $latest_version, '<')) {
-                $update = (object) [
-                    'new_version' => '1.0.1',
-                    'slug'        => plugin_basename(__FILE__),
-                    'package'     => $release_data['zipball_url'],
-                    'url'         => $plugin_data['PluginURI'],
-                    'requires'    => '5.0', // Minimum WordPress version required
-                    'tested'      => '6.3.2', // WordPress version tested up to
-                ];
-            }
-        }
+        //     // Compare the current version with the latest version
+        //     if (version_compare($current_version, $latest_version, '<')) {
+        //         $update = (object) [
+        //             'new_version' => '1.0.1',
+        //             'slug'        => plugin_basename(__FILE__),
+        //             'package'     => $release_data['zipball_url'],
+        //             'url'         => $plugin_data['PluginURI'],
+        //             'requires'    => '5.0', // Minimum WordPress version required
+        //             'tested'      => '6.3.2', // WordPress version tested up to
+        //         ];
+        //     }
+        // }
     }
 
-    return $update;
+    // return $update;
 }, 10, 4);
 
 /*
@@ -65,7 +65,7 @@ add_filter('plugin_row_meta', function ($plugin_meta, $plugin_file, $plugin_data
     if ($plugin_file == plugin_basename(__FILE__) && current_user_can('install_plugins')) {
         $plugin_meta[] = sprintf(
             '<a href="%s" aria-label="%s">%s</a>',
-            'https://github.com/CruelDrool/4cc70b819a33793396456c5ddb81781d',
+            'https://github.com/aliajjoub2/test-ionos/blob/main/test-ionos.php',
             sprintf(__('View GitHub repository for %s'), $plugin_data['Name']),
             __('View GitHub Repository')
         );
